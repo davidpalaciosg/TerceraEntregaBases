3c7e7ce055ef51abd4e1d26291347b1689033032
David Enrique Palacios Garcia <paladavid@hotmail.com>
2021-11-03 10:16:50 -0500

    First file

---
+++ Entrega.sql
@@ -0,0 +1,8 @@
+/*
+Tercera Entrega: Bases de datos
+Grupo: 4
+Nombres: David Palacios, Sebastian Vergara, Nicolas Sanchez, Maria Camila Paternina, Paula Penuela
+Cuentas: is562815, is562814,is562812,is562806,is562810
+*/
+
+--CREACIÓN DE TABLAS
